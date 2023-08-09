aa1ce0eeea7f992f50aec82da1299f2dfcf356aa
vaibhav-shaw <82948272+vaibhav-shaw@users.noreply.github.com>
2023-08-09 23:26:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=bef4b6e4-0fbb-4e11-9410-6e16e2ca5ab1&offset=0")
+        .then(data => {
+            return data.json()
+        })
+        .then(data => {
+            if (data.status != "success") throw new Error("Error");
+
+            const matchesList = data.data;
+
+            if (!matchesList) return [];
+
+            //add your api key from cricketdata.org
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            // console.log({ relevantData });
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e.message));
+}
+
+getMatchData();
+
+//https://rapidapi.com/api-sports/api/api-football/
